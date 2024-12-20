9d51e463ef2426883f88f7fde4953c6ef4a589e0
Jiali Cheng <20452970+chengjiali@users.noreply.github.com>
2024-12-20 05:59:19 -0500

    add trainer factory function

--- mubench/__init__.py
+++ mubench/__init__.py
@@ -2,7 +2,29 @@ from .args import UnlearningArguments
 from .data.base import load_unlearn_data
 from .utils import load_base_model
 
+from .bad_teaching import BadTeachingTrainer
+from .grad_ascent import GradAscentTrainer
+from .random_labeling import RandomLabelingTrainer
+from .scrub import SCRUBTrainer
+from .salun import SalUnTrainer
 
+
+def unlearn_trainer(trainer_name):
+    trainer_methods = {
+        "bad_teaching": BadTeachingTrainer,
+        "grad_ascent": GradAscentTrainer,
+        "neggrad": GradAscentTrainer,
+        "random_labeling": RandomLabelingTrainer,
+        "scrub": SCRUBTrainer,
+        "salun": SalUnTrainer,
+    }
+
+    # Check if the trainer_name exists in the dictionary
+    if trainer_name in trainer_methods:
+        return trainer_methods[trainer_name]
+    else:
+        # Raise an error if the trainer_name is not recognized
+        raise ValueError(f"Trainer '{trainer_name}' not found. Available trainers are: {', '.join(trainer_methods.keys())}")
 
 
 ## Mapping of base model names (short name to full name in HuggingFace)
