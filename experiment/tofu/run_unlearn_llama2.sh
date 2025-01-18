fe72765c077be14fc4eff4ec031727c28da97edc
Jiali Cheng <20452970+chengjiali@users.noreply.github.com>
2025-01-17 19:12:25 -0500

    add tofu llama2

---
+++ experiment/tofu/run_unlearn_llama2.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+declare -a backbone=('llama2-7b')
+declare -a method=('neggrad' 'random_label' 'grad_diff' 'npo')
+declare -a dfratio=('1' '5' '10')
+declare -a seed=('42' '87' '21' '13' '100')
+
+d='tofu'
+
+set -e
+
+for b in "${backbone[@]}"
+do
+    for m in "${method[@]}"
+    do
+        for df in "${dfratio[@]}"
+        do
+            # for sd in "${seed[@]}"
+            # do
+            sd=42
+            if [ ! -f ../../checkpoint/unlearn/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+                echo ../../checkpoint/unlearn/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+                WANDB_MODE=offline CUDA_VISIBLE_DEVICES=4,5 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn/"${b}"/"${m}"/"${df}"/"${sd}".json &
+            fi
+
+            sd=87
+            if [ ! -f ../../checkpoint/unlearn/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+                echo ../../checkpoint/unlearn/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+                WANDB_MODE=offline CUDA_VISIBLE_DEVICES=6,7 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn/"${b}"/"${m}"/"${df}"/"${sd}".json &
+            fi
+
+            trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+            wait
+            # done
+        done
+    done
+done
+
+# Unlearn CL
+for b in "${backbone[@]}"
+do
+    for m in "${method[@]}"
+    do
+        # for df in "${dfratio[@]}"
+        # do
+            # for sd in "${seed[@]}"
+            # do
+        sd=42
+        df=1
+        if [ ! -f ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+            echo ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+            WANDB_MODE=offline CUDA_VISIBLE_DEVICES=2,3 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn_cl/"${b}"/"${m}"/"${df}"/"${sd}".json &
+        fi
+
+        df=5
+        if [ ! -f ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+            echo ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+            WANDB_MODE=offline CUDA_VISIBLE_DEVICES=4,5 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn_cl/"${b}"/"${m}"/"${df}"/"${sd}".json &
+        fi
+
+        df=10
+        if [ ! -f ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+            echo ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+            WANDB_MODE=offline CUDA_VISIBLE_DEVICES=6,7 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn_cl/"${b}"/"${m}"/"${df}"/"${sd}".json &
+        fi
+
+        # sd=87
+        # if [ ! -f ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}"/all_results.json ]; then
+        #     echo ../../checkpoint/unlearn_cl/"${d}"/"${b}"/"${m}"/"${df}"/"${sd}" "Not Found"
+        #     WANDB_MODE=offline CUDA_VISIBLE_DEVICES=7 PYTHONPATH=../..:$PYTHONPATH python unlearn.py configs/unlearn_cl/"${b}"/"${m}"/"${df}"/"${sd}".json &
+        # fi
+
+        trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+        wait
+        # done
+        done
+    done
+done
